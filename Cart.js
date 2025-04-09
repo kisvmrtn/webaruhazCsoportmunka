def3d044b6c5f1cd171536bfba6200244dad0393
Kisváradi Márton (Szf_N_2024_2026) <kisvaradi.marci@gmail.com>
2025-04-09 14:25:21 +0200

    shopcart kezdetleges

--- Cart.js
+++ Cart.js
@@ -5,13 +5,51 @@ export default class Cart {
     constructor(list, pElem) {
         this.#list = list;
         this.pElem = pElem;
-        this.addEvent()
+        this.counter = 0;
+        this.shopElem = document.querySelector('.shopCart');
+        this.shopIcon = document.querySelector('.bi');
+        this.addEvent();
+        this.show();
     }
     addEvent() {
         window.addEventListener("add", (event) => {
             this.#list.push(event.detail);
             console.log(this.#list)
+            this.counter++
+            console.log(this.counter)
+            this.shopIcon.innerHTML = this.counter
         });
     }
 
+    show() {
+
+        let sidebar = document.querySelector('.sidebar');
+        this.shopElem.addEventListener("click", () => {
+            switch (sidebar.style.display) {
+                case "none":
+                    sidebar.style.display = "flex"
+                    sidebar.innerHTML = ""
+                    break;
+                case "flex":
+                    sidebar.style.display = "none"
+                    break;
+                default:
+                    sidebar.style.display = "flex"
+            }
+            for (let i = 0; i < this.#list.length; i++) {
+                console.log(this.#list)
+                let html = `
+                    <div>
+                        <p><img src="${this.#list[i].img}" alt="" style="max-width: 75px"></p>
+                        <p>${this.#list[i].name}</p>
+                        <p>${this.#list[i].price} Ft</p>
+                        <button class="delete">X</button>
+                        <hr>
+                    </div>
+                    
+                `
+                sidebar.innerHTML += html
+            }
+        })
+    }
 }
