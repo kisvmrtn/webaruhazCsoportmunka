808ff51a43826584636604c54c9aa08dba14cd2f
5t3x <kissmatemail@gmail.com>
2025-04-09 11:12:08 +0200

    index kezdetleges

---
+++ ProductList.js
@@ -0,0 +1,32 @@
+export const productList = [
+    {
+        id:1,
+        name:"Laptop",
+        price: 200000,
+        img: "images/laptop.jpg"
+    },
+    {
+        id: 2,
+        name:"Smartphone",
+        price: 389990,
+        img: "images/smartphone.jpg"
+    },
+    {
+        id: 3, 
+        name: "Tablet",
+        price: 19990,
+        img: "images/tablet.jpg"
+    },
+    {
+        id: 4,
+        name: "Monitor",
+        price: 74980,
+        img: "images/monitor.jpg"
+    },
+    {
+        id: 5,
+        name: "Headphones",
+        price: 44990,
+        img: "images/headphones.jpg"
+    }
+];
